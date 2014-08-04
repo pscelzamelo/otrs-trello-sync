c5333571ed65cb0bf806833376c2457b9100f217
Pedro Melo <pscelzamelo@gmail.com>
2014-08-04 17:55:13 -0300

    Manually adding ignored config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    trelloKey : "trello-key",
+    trelloToken: "trello-token",
+    trelloOrganizationName: "trello-organization",
+    otrsBaseAddress: "http://otrs-host/otrs/",
+    otrsUser: "otrs-user",
+    otrsPassword: "otrs-password",
+}
